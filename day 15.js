f644eda25d653f1ccdf3881d1e822fa4d95c4d28
Sarthak Badola <109479633+5arthak8adola@users.noreply.github.com>
2023-01-26 22:33:42 +0530

    Add files via upload

---
+++ day 15.js
@@ -0,0 +1,48 @@
+{
+    "name": "app_name",
+    "version": "1.0.0",
+    "private": true,
+    "scripts": {
+      "dev": "next dev",
+      "build": "next build",
+      "start": "next start",
+      "lint": "next lint"
+    },
+    "browserslist": [
+      "chrome 90",
+      "edge 90",
+      "firefox 90",
+      "opera 90",
+      "safari 14"
+    ],
+    "dependencies": {
+      "@next/font": "latest",
+      "@react-icons/all-files": "^4.1.0",
+      "firebase": "^9.14.0",
+      "next": "^13.1.5",
+      "next-plugin-preact": "^3.0.7",
+      "nprogress": "^0.2.0",
+      "preact": "^10.11.3",
+      "preact-render-to-string": "^5.2.6",
+      "react": "npm:@preact/compat@^17.1.2",
+      "react-dom": "npm:@preact/compat@^17.1.2",
+      "react-hot-toast": "^2.4.0",
+      "react-ssr-prepass": "npm:preact-ssr-prepass@^1.2.0",
+      "sharp": "^0.31.3"
+    },
+    "devDependencies": {
+      "@next/bundle-analyzer": "^13.1.2",
+      "@types/node": "^18.11.9",
+      "@types/nprogress": "^0.2.0",
+      "@types/react": "^18.0.25",
+      "@typescript-eslint/eslint-plugin": "^5.46.0",
+      "autoprefixer": "^10.4.13",
+      "eslint": "^8.28.0",
+      "eslint-config-next": "^13.0.5",
+      "postcss": "^8.4.19",
+      "prettier": "^2.8.0",
+      "prettier-plugin-tailwindcss": "^0.2.0",
+      "tailwindcss": "^3.2.4",
+      "typescript": "^4.9.3"
+    }
+  }
